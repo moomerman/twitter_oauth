409fed458ce98eea5e486d985515fbe32e15741c
Uriah Welcome <precision@devrandom.net>
2009-03-29 19:56:25 -0400

    adding friendship api access

---
+++ lib/twitter_oauth/friendships.rb
@@ -0,0 +1,28 @@
+module TwitterOAuth
+  class Client
+    
+    def friends
+      oauth_response = access_token.post("friends/ids.json")
+      JSON.parse(oauth_response.body)
+    end
+    
+    # friend this user.
+    def friend(id)
+      oauth_response = access_token.post("/friendships/create/#{id}.json")
+      JSON.parse(oauth_response.body)
+    end
+    
+    # unfriend.
+    def unfriend(id)
+      oauth_response = access_token.post("/friendships/destroy/#{id}.json")
+      JSON.parse(oauth_response.body)
+    end
+    
+    # exists?.
+    def exists?(a, b)
+      oauth_response = access_token.get("/friendships/exists.json?user_a=#{a}&user_b=#{b}")
+      JSON.parse(oauth_response.body)
+    end
+    
+  end
+end
