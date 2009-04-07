0e0adef434662c79eb52a0c876acb1b91b7fde89
Richard Taylor <moomerman@gmail.com>
2009-04-07 16:04:16 +0100

    added friends list and bumped gem version

--- lib/twitter_oauth/user.rb
+++ lib/twitter_oauth/user.rb
@@ -1,6 +1,12 @@
 module TwitterOAuth
   class Client
      
+    # Returns the 100 last friends
+    def friends(page=1)
+      oauth_response = access_token.get("/statuses/friends.json?page=#{page}")
+      JSON.parse(oauth_response.body)
+    end 
+    
     # Returns the 100 last followers
     def followers(page=1)
       oauth_response = access_token.get("/statuses/followers.json?page=#{page}")
