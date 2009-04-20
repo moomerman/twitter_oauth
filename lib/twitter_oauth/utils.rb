d98b34418d50fa9e1ce231855a8246d2b94c3d45
Trek Glowacki <pietrekg@umich.edu>
2009-04-21 02:24:36 +0800

    Removed debugging puts
    Signed-off-by: Richard Taylor <moomerman@gmail.com>

--- lib/twitter_oauth/utils.rb
+++ lib/twitter_oauth/utils.rb
@@ -7,14 +7,14 @@ module TwitterOAuth
       def http_multipart_data(params)
         body    = ""
         headers = {}
-
+        
         boundary = Time.now.to_i.to_s(16)
-
+        
         headers["Content-Type"] = "multipart/form-data; boundary=#{boundary}"
         params.each do |key,value|
           esc_key = OAuth::Helper.escape(key.to_s)
           body <<  "--#{boundary}#{CRLF}"
-
+          
           if value.respond_to?(:read)
             mime_type = MIME::Types.type_for(value.path)[0] || MIME::Types["application/octet-stream"][0]
             body << "Content-Disposition: form-data; name=\"#{esc_key}\"; filename=\"#{File.basename(value.path)}\"#{CRLF}"
@@ -24,12 +24,10 @@ module TwitterOAuth
             body << "Content-Disposition: form-data; name=\"#{esc_key}\"#{CRLF*2}#{value}"
           end
         end
-      
+        
         body << "--#{boundary}--#{CRLF*2}"
         headers["Content-Length"] = body.size.to_s
-      
-        puts body.inspect
-        puts headers.inspect
+        
         return [ body, headers ]
       end
   end
