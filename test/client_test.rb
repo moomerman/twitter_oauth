ca8e7fb74c36ac8f2e03760e0273692078b0634e
Corey Grusden <corey.grusden@gmail.com>
2009-06-06 21:22:40 -0400

    Added basic coverage for Client#authorize

--- test/client_test.rb
+++ test/client_test.rb
@@ -1,4 +1,5 @@
 require File.join(File.dirname(__FILE__), "/test_helper")
+require 'mocha'
 
 class ClientTest < Test::Unit::TestCase
   include TwitterOAuth
@@ -11,6 +12,19 @@ class ClientTest < Test::Unit::TestCase
 
     should "be kind of TwitterOAuth" do
       assert_kind_of TwitterOAuth::Client, @client
+    end
+
+    context "when authorizing" do
+      setup do
+        @request_token = stub('a request token')
+        @access_token = stub_everything('access token')
+        @request_token.stubs(:get_access_token).returns(@access_token)
+        OAuth::RequestToken.stubs(:new).returns(@request_token)
+      end
+
+      should "return an access token" do
+        assert_equal @client.authorize("haha","haha"), @access_token
+      end
     end
 
   end
