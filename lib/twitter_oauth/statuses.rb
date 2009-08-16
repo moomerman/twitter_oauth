898aefb8289044bb26d034e9f8173d1afbdefa51
Richard Taylor <moomerman@gmail.com>
2009-08-16 16:25:35 +0100

    implemented upcoming retweet api calls, version 0.2.0

--- lib/twitter_oauth/statuses.rb
+++ lib/twitter_oauth/statuses.rb
@@ -7,6 +7,13 @@ module TwitterOAuth
       JSON.parse(oauth_response.body)
     end
     
+    # Returns the 20 most recent statuses, including retweets, posted by the authenticating user and that user's friends. 
+    # This is the equivalent of /timeline/home on the Web.
+    def home_timeline
+      oauth_response = access_token.get("/statuses/home_timeline.json")
+      JSON.parse(oauth_response.body)
+    end
+    
     # Returns the 20 most recent statuses posted by the authenticating user and that user's friends.
     def friends_timeline(rpp=20, page=1)
       oauth_response = access_token.get("/statuses/friends_timeline.json?count=#{rpp}&page=#{page}")
@@ -34,6 +41,30 @@ module TwitterOAuth
     # Returns the 20 most recent @replies (status updates prefixed with @username) for the authenticating user.
     def replies(page=1)
       oauth_response = access_token.get("/statuses/mentions.json?page=#{page}")
+      JSON.parse(oauth_response.body)
+    end
+    
+    # Retweets the tweet specified by the id parameter. Returns the original tweet with retweet details embedded.
+    def retweet(id)
+      oauth_response = access_token.post("/statuses/retweet/#{id}.json")
+      JSON.parse(oauth_response.body)
+    end
+    
+    # Returns the 20 most recent retweets posted by the authenticating user
+    def retweeted_by_me
+      oauth_response = access_token.get("/statuses/retweeted_by_me.json")
+      JSON.parse(oauth_response.body)
+    end
+    
+    # Returns the 20 most recent retweets posted by the authenticating user's friends.
+    def retweeted_to_me
+      oauth_response = access_token.get("/statuses/retweeted_to_me.json")
+      JSON.parse(oauth_response.body)
+    end
+    
+    # Returns the 20 most recent tweets of the authenticated user that have been retweeted by others.
+    def retweets_of_me
+      oauth_response = access_token.get("/statuses/retweets_of_me.json")
       JSON.parse(oauth_response.body)
     end
     
