738ae8a93beda1aa692cf6bf367ab41e62170bac
Richard Taylor <moomerman@gmail.com>
2009-12-29 19:07:41 +0000

    refactored the search methods to use helper and added user agent as suggested for ec2 environment

--- lib/twitter_oauth/search.rb
+++ lib/twitter_oauth/search.rb
@@ -1,5 +1,4 @@
 require 'open-uri'
-require 'ostruct'
 
 module TwitterOAuth
   class Client
@@ -7,30 +6,28 @@ module TwitterOAuth
     def search(q, options={})
       options[:page] ||= 1
       options[:per_page] ||= 20
-      response = open("http://search.twitter.com/search.json?q=#{URI.escape(q)}&page=#{options[:page]}&rpp=#{options[:per_page]}&since_id=#{options[:since_id]}")
-      search_result = JSON.parse(response.read)
-      search_result = OpenStruct.new(search_result)
-      search_result.results = search_result.results.collect{|x| OpenStruct.new(x)}
-      search_result
+      search_get("http://search.twitter.com/search.json?q=#{URI.escape(q)}&page=#{options[:page]}&rpp=#{options[:per_page]}&since_id=#{options[:since_id]}")
     end
     
     # Returns the current top 10 trending topics on Twitter.
     def current_trends
-      response = open("http://search.twitter.com/trends/current.json")
-      JSON.parse(response.read)
+      search_get("http://search.twitter.com/trends/current.json")
     end
     
     # Returns the top 20 trending topics for each hour in a given day.
     def daily_trends
-      response = open("http://search.twitter.com/trends/daily.json")
-      JSON.parse(response.read)
+      search_get("http://search.twitter.com/trends/daily.json")
     end
     
     # Returns the top 30 trending topics for each day in a given week.
     def weekly_trends
-      response = open("http://search.twitter.com/trends/weekly.json")
-      JSON.parse(response.read)
+      search_get("http://search.twitter.com/trends/weekly.json")
     end
     
+    private
+      def search_get(url)
+        response = open(url, 'User-Agent' => 'github.com/moomerman/twitter_outh')
+        JSON.parse(response.read)
+      end
   end
 end
