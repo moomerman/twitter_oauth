efdfaa262da77d6425c9c639daeacb886fb00586
Richard Taylor <moomerman@gmail.com>
2012-10-12 12:05:54 +0100

    using the https endpoint for all oauth negotiation

--- lib/twitter_oauth/client.rb
+++ lib/twitter_oauth/client.rb
@@ -17,57 +17,60 @@ require 'twitter_oauth/geo'
 
 module TwitterOAuth
   class Client
-    
+
     def initialize(options = {})
       @consumer_key = options[:consumer_key]
       @consumer_secret = options[:consumer_secret]
       @token = options[:token]
       @secret = options[:secret]
       @proxy = options[:proxy]
+      @debug = options[:debug]
     end
-  
+
     def authorize(token, secret, options = {})
       request_token = OAuth::RequestToken.new(
-        consumer, token, secret
+        consumer(:secure => true), token, secret
       )
       @access_token = request_token.get_access_token(options)
       @token = @access_token.token
       @secret = @access_token.secret
       @access_token
     end
-  
+
     def show(username)
       get("/users/show/#{username}.json")
     end
-    
+
     # Returns the string "ok" in the requested format with a 200 OK HTTP status code.
     def test
       get("/help/test.json")
     end
-    
+
     def request_token(options={})
-      consumer.get_request_token(options)
+      consumer(:secure => true).get_request_token(options)
     end
-    
+
     def authentication_request_token(options={})
-      consumer.options[:authorize_path] = '/oauth/authenticate'
+      consumer(:secure => true).options[:authorize_path] = '/oauth/authenticate'
       request_token(options)
     end
-    
+
     private
-    
-      def consumer
+
+      def consumer(options={})
+        options[:secure] ||= false
+        protocol = options[:secure] ? 'https' : 'http'
         @consumer ||= OAuth::Consumer.new(
           @consumer_key,
           @consumer_secret,
-          { :site => 'http://api.twitter.com', :request_endpoint => @proxy }
+          { :site => "#{protocol}://api.twitter.com", :request_endpoint => @proxy }
         )
       end
 
       def access_token
         @access_token ||= OAuth::AccessToken.new(consumer, @token, @secret)
       end
-      
+
       def get(path, headers={})
         headers.merge!("User-Agent" => "twitter_oauth gem v#{TwitterOAuth::VERSION}")
         oauth_response = access_token.get("/1#{path}", headers)
@@ -87,4 +90,4 @@ module TwitterOAuth
       end
   end
 end
-   
+
